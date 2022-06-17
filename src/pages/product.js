54fc5278acb66ac102db51a414e69ef85d6076d7
vasil-yordanov14 <vasil_sh@abv.bg>
2022-06-17 19:46:40 +0300

    comfy store

---
+++ src/pages/product.js
@@ -0,0 +1,7 @@
+// global imports
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
+// specific
+import { addToCart } from "../cart/setupCart.js";
+import { singleProductUrl, getElement, formatPrice } from "../utils.js";
